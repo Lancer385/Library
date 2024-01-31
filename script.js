f4ca42b5692d8270107f510f3d5e9abee9fa0ab0
lancer385 <ahmedkhalaf1234123@gmail.com>
2024-01-31 17:38:22 +0200

    update

--- script.js
+++ script.js
@@ -1,37 +1,82 @@
-const library = document.querySelector("#library");
-
 function Book(title, author, pages, read){
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = ()=>{
-    return `${this.title}. ${this.author}, ${this.pages}`
-  };
 };
-const book = new Book("The Elegance of the Hedgehog", 'Muriel Barbery', 256, "not read");
-const myLibrary =[book];
-
 
 function addBookToLibrary(){
-  let promptUser = prompt("enter the book name", "harry potter");
-  myLibrary.push(promptUser);
+    for (i = 0; i < myLibrary.length; i++){
+    let div = document.createElement("div");
+      div.setAttribute("data-index",i);
+      let book = {
+        title : document.createElement("div"),
+        author : document.createElement("div"),
+        pages :document.createElement("div"),
+        readBtn : document.createElement("button"),
+        delBtn : document.createElement("button")
+      };
+      book.readBtn.setAttribute("class", "read");
+      book.delBtn.setAttribute("class","delete");
+      book.title.innerText = `Name: ${myLibrary[i].title}`;
+      book.author.innerText = `Author: ${myLibrary[i].author}`;
+      book.pages.innerText = `Pages: ${myLibrary[i].pages}`
+      book.readBtn.innerText = myLibrary[i].read;
+      book.delBtn.innerText = "Delete";
+      library.appendChild(div);
+      console.log(myLibrary[i]);
+      for (const e in book){
+        div.appendChild(book[e]);
+      };
+      let j = i;
+      book.readBtn.addEventListener("click", ()=>{
+          if (book.readBtn.innerText == "Read"){
+            book.readBtn.innerText = "Not Read";
+            myLibrary[j].read = "Not Read"
+          }
+          else if (book.readBtn.innerText == "Not Read"){
+            book.readBtn.innerText = "Read";
+            myLibrary[j].read = "Read";
+          };
+      })
+      book.delBtn.addEventListener("click", ()=> {
+        myLibrary.splice(book.delBtn.parentNode.getAttribute("data-index"), 1);
+        book.delBtn.parentNode.remove();
+        removeChilds();
+        addBookToLibrary();
+        console.log(myLibrary);
+      })
+    };
 };
 
-function displayBooks(){
-  let arrayLength = myLibrary.length;
-  for (i = 0; i < arrayLength; i++){
-    let div = document.createElement("div");
-    let btn = document.createElement("button");
-    div.innerText = myLibrary[i].info();
-    btn.innerText = myLibrary[i].read;
-    library.appendChild(div);
-    div.appendChild(btn);
+function removeChilds(){
+  for(i = 0; i < myLibrary.length; i++){
+    library.removeChild(library.firstElementChild);
   }
 }
-displayBooks();
-const addBook = document.querySelector("#newBook")
+
+
+const library = document.querySelector("#library");
+let book = new Book("The Elegance of the Hedgehog", 'Muriel Barbery', 256, "Not Read");
+const myLibrary =[book];
+const dialog = document.querySelector("dialog");
+const addBook = document.querySelector("#newBook");
+const form = document.querySelector("form");
+const exitBtn = document.querySelector("#exit")
+addBookToLibrary();
+
 addBook.addEventListener('click', ()=>{
+  dialog.showModal();
+});
+exitBtn.addEventListener('click', ()=>{
+  dialog.close();
+});
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  let book = new Book(document.querySelector("#title").value, document.querySelector("#author").value, document.querySelector("#pages").value, document.querySelector('input[name="read"]:checked').value);
+  removeChilds();
+  myLibrary.push(book);
   addBookToLibrary();
-  displayBooks();
+  dialog.close();
+  console.log(myLibrary);
 });
